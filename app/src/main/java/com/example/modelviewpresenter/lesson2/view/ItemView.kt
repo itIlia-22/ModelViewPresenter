d6c8fe03b464fad53f6d023a322eddb9cec567aa
илья бугай <bugai.samara@yandex.ru>
2022-08-09 18:43:47 +0400

    lesson/dz 2. Cоздал Интерфейс для элементов по позиции и сам интерфейс элементов

---
+++ app/src/main/java/com/example/modelviewpresenter/lesson2/view/ItemView.kt
@@ -0,0 +1,5 @@
+package com.example.modelviewpresenter.lesson2.view
+
+interface ItemView {
+    var position:Int
+}
---
+++ app/src/main/java/com/example/modelviewpresenter/lesson2/view/UserItemView.kt
@@ -0,0 +1,5 @@
+package com.example.modelviewpresenter.lesson2.view
+
+interface UserItemView:ItemView {
+    fun setLoginUser(text:String)
+}
