486968e69347b374585361eb004557d54467dd0e
илья бугай <bugai.samara@yandex.ru>
2022-08-18 18:10:57 +0400

    andThen i Single

--- app/src/main/java/com/example/modelviewpresenter/RxJava2.kt
+++ app/src/main/java/com/example/modelviewpresenter/RxJava2.kt
@@ -1,29 +1,48 @@
 package com.example.modelviewpresenter
 
 import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
 import java.lang.Exception
 
-fun main(){
+private val dataF = listOf<String>("Vse Okey", "Ne okey")
+
+fun main() {
 
     saveCompletable("email.com")
-        .subscribe{
-            println("Completable")
-        }
+        .andThen(newNews())
+        .subscribe({
+            println("Single subscribe")
+            println(it)
+        }, {
+            println(it.message)
+        })
 
 }
 
-private fun saveCompletable(email:String) = Completable.create{emitter->
+private fun newNews() = Single.create<List<String>> { emitter ->
+    try {
+        val result = dataF
+        emitter.onSuccess(result)
+
+
+    } catch (e: Exception) {
+        emitter.onError(e)
+
+    }
+}
+
+private fun saveCompletable(email: String) = Completable.create { emitter ->
     try {
         saveBD(email)
         emitter.onComplete()
-    }catch (e:Exception){
+    } catch (e: Exception) {
         emitter.onError(e)
 
 
     }
 }
 
-private fun saveBD(email:String){
+private fun saveBD(email: String) {
     Thread.sleep(1000L)
     println("save: $email")
 
