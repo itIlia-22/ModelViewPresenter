4a947d69eabd525d58667454744e686b579a015e
илья бугай <bugai.samara@yandex.ru>
2022-08-18 18:47:55 +0400

    Hot

---
+++ app/src/main/java/com/example/modelviewpresenter/rxlava/RxJavaAdvanced.kt
@@ -0,0 +1,18 @@
+package com.example.modelviewpresenter.rxlava
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.observables.ConnectableObservable
+
+
+fun main(){
+  val hot =   createHotObs()
+        hot.subscribe{
+            println("value = $it")
+        }
+    hot.connect()
+
+}
+
+ fun createHotObs():ConnectableObservable<Int> =
+     Observable.just(12,312,13,12)
+         .publish()
