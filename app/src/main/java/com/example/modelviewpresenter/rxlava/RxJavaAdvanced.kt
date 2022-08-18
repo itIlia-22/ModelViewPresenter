1b7be00f4e9d2e93858cddb8ca86df40bd0e86d3
илья бугай <bugai.samara@yandex.ru>
2022-08-18 18:50:13 +0400

    Hot 2var

--- app/src/main/java/com/example/modelviewpresenter/rxlava/RxJavaAdvanced.kt
+++ app/src/main/java/com/example/modelviewpresenter/rxlava/RxJavaAdvanced.kt
@@ -4,15 +4,21 @@ import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.observables.ConnectableObservable
 
 
-fun main(){
-  val hot =   createHotObs()
-        hot.subscribe{
-            println("value = $it")
-        }
+fun main() {
+    val hot = createHotObs()
+    hot.subscribe {
+        println("value = $it")
+    }
+
+    Thread.sleep(1000)
+
+    hot.subscribe {
+        println("second = $it")
+    }
     hot.connect()
 
 }
 
- fun createHotObs():ConnectableObservable<Int> =
-     Observable.just(12,312,13,12)
-         .publish()
+fun createHotObs(): ConnectableObservable<Int> =
+    Observable.just(12, 312, 13, 12)
+        .publish()
